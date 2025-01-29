ba4e60d97ff41f71090dba295ad0ef5223ed2fc9
Raeid-U <raeidus15@gmail.com>
2025-01-29 00:16:49 -0500

    revised structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,16 @@
-'use client'
-import React, { useState } from 'react';
-import TimerTab from './components/TimerTab';
-import Timer from './components/Timer';
-import Log from './components/Log';
-import MusicPlayer from './components/MusicPlayer';
-import { FaHistory } from 'react-icons/fa';
-import dayjs from 'dayjs';
+"use client";
+import React, { useState } from "react";
+import TimerTab from "../components/TimerTab";
+import Timer from "../components/Timer";
+import Log from "../components/Log";
+import MusicPlayer from "../components/MusicPlayer";
+import dayjs from "dayjs";
+import { FaHistory } from "react-icons/fa";
 
 const timers = [
-  { name: 'Pomodoro Sprint', duration: 25, bg: 'bg-pomodoro' },
-  { name: 'Small Break', duration: 1, bg: 'bg-small-break' },
-  { name: 'Long Break', duration: 15, bg: 'bg-long-break' },
+  { name: "Pomodoro Sprint", duration: 25, bg: "bg-pomodoro" },
+  { name: "Small Break", duration: 1, bg: "bg-small-break" },
+  { name: "Long Break", duration: 15, bg: "bg-long-break" },
 ];
 
 const Home = () => {
@@ -25,8 +25,8 @@ const Home = () => {
 
   const handleTimerComplete = (timer) => {
     const endTime = dayjs();
-    const startTimeFormatted = startTime ? startTime.format('h:mm A') : '';
-    const endTimeFormatted = endTime.format('h:mm A');
+    const startTimeFormatted = startTime ? startTime.format("h:mm A") : "";
+    const endTimeFormatted = endTime.format("h:mm A");
 
     setLog((prevLog) => [
       {
@@ -45,7 +45,9 @@ const Home = () => {
   };
 
   return (
-    <div className={`min-h-screen flex flex-col items-center justify-center transition-all duration-500 ease-in-out ${selectedTimer.bg}`}>
+    <div
+      className={`min-h-screen flex flex-col items-center justify-center transition-all duration-500 ease-in-out ${selectedTimer.bg}`}
+    >
       <div className="w-full max-w-lg p-4 relative">
         <div className="tabs flex justify-center mb-4">
           {timers.map((timer, index) => (
@@ -58,10 +60,17 @@ const Home = () => {
           ))}
         </div>
         <div className="blurred-gui relative p-4 bg-white bg-opacity-20 backdrop-blur-lg rounded-lg">
-          <div className="log-icon absolute top-4 right-4 text-gray-200 hover:text-white cursor-pointer" onClick={toggleLog}>
+          <div
+            className="log-icon absolute top-4 right-4 text-gray-200 hover:text-white cursor-pointer"
+            onClick={toggleLog}
+          >
             <FaHistory size={24} />
           </div>
-          <Timer timer={selectedTimer} onStart={handleTimerStart} onComplete={handleTimerComplete} />
+          <Timer
+            timer={selectedTimer}
+            onStart={handleTimerStart}
+            onComplete={handleTimerComplete}
+          />
           <MusicPlayer />
         </div>
       </div>
